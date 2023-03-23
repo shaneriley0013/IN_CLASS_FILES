65d10809916689b5003bc6c95f567a13623c8cb4
SHANE RILEY <shaneriley0013@gmail.com>
2023-03-23 15:42:27 -0500

    add input, roll method, score method

---
+++ day_4/bowling_exercize/bowling_score.rb
@@ -0,0 +1,23 @@
+
+
+
+
+# define names?? 
+# take in score for each roll
+# add scores from first frame
+# add total score
+# make exception for strike
+# make exception for spare
+
+
+puts "What was your score"
+score = gets.chomp.to_i
+
+
+def roll
+end
+
+def score
+end
+
+
