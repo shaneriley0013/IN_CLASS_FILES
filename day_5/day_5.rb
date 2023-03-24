62f1911cb1537c1c327e232ee54ff2df6791a5d8
SHANE RILEY <shaneriley0013@gmail.com>
2023-03-24 12:31:18 -0500

    add code

--- day_5/day_5.rb
+++ day_5/day_5.rb
@@ -8,3 +8,13 @@ require 'http'
 
 response = HTTP.get("https://data.cityofchicago.org/resource/xzkq-xp2w.json")
 
+employees = response.parse(:json)
+
+#pp employees.length
+
+i = 0
+employees.each do |salary|
+  pp employees[1]["salary_or_hourly"]
+end
+
+# pp employees[1]["salary_or_hourly"]
